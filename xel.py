808e6bbc38b9843e84cff7738ea5cf098b9cf11f
Rick Sanchez <71662588+Rick-Man-God@users.noreply.github.com>
2020-09-22 13:32:06 +0200

    Syntax Error (fix)
    Syntax Error: invalid syntax'

--- xel.py
+++ xel.py
@@ -9,7 +9,7 @@ import random
 import time
 
 BASE58 = '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-poss = ['1337', '1337-H4X0R']
+poss = ['rick']
 
 address = ''
 j = 0
@@ -19,12 +19,12 @@ url_pw = 'http://localhost:17876/nxt?=%2Fnxt&requestType=getAccountId&secretPhra
 
 stime = time.time()
 found = False
-while not found
+while not found:
 	passphrase =  ('%s%s' % ('S', ''.join([BASE58[ random.SystemRandom().randrange(0,len(BASE58)) ] for i in range(34)])))
 	address = requests.get(url_pw.format(passphrase)).json()['accountRS']
 
 	j += 1
-	print(address, j, passphrase, bal)
+	print(address, j, passphrase)
 	
 	if any(str in address for str in poss):
 		found = True
